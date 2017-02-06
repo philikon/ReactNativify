3ed2d5017a65f0beed0fd30b4ced347b20a22d56
Philipp von Weitershausen <philikon@silklabs.com>
2017-02-06 14:21:15 -0800

    Polyfill NODE_ENV env var based on react-native's __DEV__ value

--- global.js
+++ global.js
@@ -1,6 +1,7 @@
 // Inject node globals into React Native global scope.
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
+global.process.env.NODE_ENV = __DEV__ ? 'development' : 'production';
 
 // Don't do this in production.
 global.crypto = {
